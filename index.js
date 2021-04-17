4cb31bcf95eccc3c0ca34004929a3c09a6612528
Urip Subekti <urip.subekti13@gmail.com>
2021-04-17 09:18:06 +0700

    init

---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+var nrc = require('node-run-cmd');
+const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+const repo = `https://github.com/subekti404dev/react-native-boilerplate.git`
+const chalk = require('chalk')
+const readline = require('readline').createInterface({
+   input: process.stdin,
+   output: process.stdout
+});
+const log = (message) => {
+   console.log(chalk.grey(message));
+}
+
+const input = async (question) => {
+   return new Promise((resolve) => {
+      return readline.question(chalk.green(question), answer => {
+         resolve(answer);
+      });
+   })
+}
+
+const createProject = async (name, bundle) => {
+   try {
+      const appDir = name.replace(' ', '');
+      log('- Cloning project...');
+      await nrc.run(`git clone ${repo} ${appDir}`);
+      
+      log('- Move to project directory...')
+      await nrc.run(`cd ${appDir}`);
+
+      log('- Installing project dependencies...')
+      await nrc.run(`npm install`);
+      
+      log('- Rename project...')
+      await nrc.run(`npx react-native-rename "${name}" -b "${bundle}"`);
+      
+      log('- Initializing git...')
+      await nrc.run(`npx rimraf .git`);
+      await nrc.run(`git init`);
+      await nrc.run(`git add .`);
+      await nrc.run(`git commit -m "Init Project"`);
+
+      console.log(chalk.green('Done'));
+      process.exit()
+   } catch (error) {
+      console.log(chalk.red(error.message));
+      process.exit()
+   }
+}
+
+const getCreateInputByProject = async () => {
+   try {
+      const name = await input("Enter App Name: ");
+      if (!name) throw new Error("App Name is required");
+      const bundle = await input("Enter Bundle Identifier (Ex: 'com.subekti.todoapp'): ");
+      if (!bundle) throw new Error("Bundle Identifier is required");
+      await createProject(name, bundle)
+   } catch (error) {
+      console.log(chalk.red(error.message));
+      process.exit()
+   }
+}
+
+if (argv.name && argv.bundle) {
+   const name = argv.name;
+   const bundle = argv.bundle;
+
+   createProject(name, bundle);
+} else {
+   getCreateInputByProject();
+}
+
